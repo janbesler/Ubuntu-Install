05b483ddc3c41823eb8c15aebc2d5ea6cd67379d
Jan Besler <73176766+janbesler@users.noreply.github.com>
2023-09-14 18:04:56 +0200

    more apps

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -13,7 +13,16 @@ List_of_Apps = "
 	kitty
 	codium
 	termius
-	vlc"
+	vlc
+ 	gnome-tweak-tools
+  	mscorefonts-installer
+   	git"
 
 apt-get update
 apt-get install -y $List_of_Apps
+
+apt autoremove -y
+apt autoclean -y
+
+git config --global user.name "janbesler"
+git config --global user.email jan_besler@online.de
